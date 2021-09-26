91b94bc54bb3a85cde70b832cc8f45790fa6d0dc
Gustavo Maia <gustavojv_maia@outlook.com>
2021-09-26 11:15:59 -0300

    States and Functions

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -6,8 +6,26 @@ import TodoList from "./components/TodoList";
 
 
 function App() {
+  //State
   const [inputText, setInputText] = useState ("");
   const [todos, setTodos] = useState ([]);
+  const [status, setStatus] = useState ("all");
+  const [filteredTodos, setFilteredTodos] = useState ([]);
+  //Funções
+  const filterHandler = () => {
+    switch (status) {
+      case "completed":
+        setFilteredTodos (todos.filter(todo => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredTodos (todos.filter(todo => todo.completed === false));
+        break;
+        default:
+          setFilteredTodos(todos);
+          break;
+    }
+  }
+
   return (
     <div className="App">
       <header>
@@ -17,7 +35,8 @@ function App() {
     inputText={inputText} 
     todos={todos} 
     setTodos={setTodos} 
-    setInputText={setInputText} 
+    setInputText={setInputText}
+    setStatus={setStatus} 
     />
     <TodoList setTodos={setTodos} todos={todos}/>
     </div>
